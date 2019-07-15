a6d78a7bc16b8c5e6bc93abf3906065311bae8ea
Arshiii <arshiii4425@gmail.com>
2019-07-15 23:03:17 +0500

    Create a responsive image gallery using react js

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,8 @@
 import React ,{Component} from 'react'
-import FluidGrid from 'react-fluid-grid'
 import './App.css';
 import galleryData from '../media.json';
 import GalleryItem from '../GalleryItem/GalleryItem';
 
-
-const styleStrategies = [
-  { mediaQuery: '(max-width: 619.9px)', style: { numberOfColumns: 1, gutterHeight: 0, gutterWidth: 0 } },
-  { mediaQuery: '(min-width: 620px) and (max-width: 719.9px)', style: { numberOfColumns: 2, gutterHeight: 0, gutterWidth: 0 } },
-  { mediaQuery: '(min-width: 720px) and (max-width: 919.9px)', style: { numberOfColumns: 3, gutterHeight: 0, gutterWidth: 0 } },
-  { mediaQuery: '(min-width: 920px) and (max-width: 1023.9px)', style: { numberOfColumns: 4, gutterHeight: 0, gutterWidth: 0 } },
-  { mediaQuery: '(min-width: 1024px)', style: { numberOfColumns: 5, gutterHeight: 0, gutterWidth: 0 } }
-]
-const transition = 'top 100ms ease-in-out, left 100ms ease-in-out';
 
 class App extends Component{
 
@@ -24,12 +14,10 @@ class App extends Component{
 
 productList = () => {
     const list = this.state.products.map((product) =>
-        <div className = "container" key ={product.id} >
-          <GalleryItem  imageUrl =  {require('../media/' + product.image )}
+          <GalleryItem key ={product.id} imageUrl =  {require('../media/' + product.image )}
           thumbnail = {require('../media/' + product.thumbnail )}
           desc = {product.description}
           />
-        </div>
     );
     return (list);
   }
@@ -37,14 +25,9 @@ productList = () => {
   render(){
     
     return(
-      <FluidGrid
-      className='my-grid'
-      itemClassName='my-grid-item'
-      styleStrategies={styleStrategies}
-      transition={transition}
-      >
-      {this.productList()}
-      </FluidGrid>
+      <div className="container-all">
+        {this.productList()};
+        </div>
     );
   }
 
--- src/GalleryItem/GalleryItem.js
+++ src/GalleryItem/GalleryItem.js
@@ -4,11 +4,9 @@ import './GalleryItem.css';
 class GalleryItem extends Component{
     render(){
         return(
-            <div className="gallery">
-                 <a href={this.props.imageUrl}>
-                     <img className = "image-item" src = {this.props.thumbnail} alt="ItemComponent"></img>
-                 </a>
-                 <span class="desc-img">{this.props.desc}</span>
+            <div className="container">
+                <img src = {this.props.thumbnail} alt=""></img>
+                <span className="title">{this.props.desc}</span>
             </div>
         );
     }
