952593155dfbcb1f26a20be5794a027cc00d50c9
Arshiii <arshiii4425@gmail.com>
2019-07-16 08:28:00 +0500

    Create a responsive image gallery using react js only without any 3rd party library

--- src/GalleryItem/GalleryItem.js
+++ src/GalleryItem/GalleryItem.js
@@ -5,7 +5,9 @@ class GalleryItem extends Component{
     render(){
         return(
             <div className="container">
-                <img src = {this.props.thumbnail} alt=""></img>
+                <a href={this.props.imageUrl}>
+                    <img src = {this.props.thumbnail} alt=""></img>
+                </a>
                 <span className="title">{this.props.desc}</span>
             </div>
         );
