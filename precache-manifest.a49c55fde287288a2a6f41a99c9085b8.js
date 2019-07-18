012fab7a395a2faf74d8691defee20fa8840fb52
Arshiii <arshiii4425@gmail.com>
2019-07-18 08:53:55 +0500

    Updates

---
+++ precache-manifest.a49c55fde287288a2a6f41a99c9085b8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9026889f7ae378c7776b9084ac14e2f7",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/index.html"
+  },
+  {
+    "revision": "acff65629a485720fcbc",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/css/main.6c2f7586.chunk.css"
+  },
+  {
+    "revision": "7e2b8530206fb761ee70",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/js/2.709c391a.chunk.js"
+  },
+  {
+    "revision": "acff65629a485720fcbc",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/js/main.5710e77c.chunk.js"
+  },
+  {
+    "revision": "173c44f428a9a6d3960f",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/js/runtime~main.b78d6953.js"
+  },
+  {
+    "revision": "ad5930d797ca8a19800d0456ed50c21a",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/1.ad5930d7.jpg"
+  },
+  {
+    "revision": "dc6402c18a9aaebd50b23b2366474cd9",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/10.dc6402c1.jpg"
+  },
+  {
+    "revision": "578275b30806cd0fe4ccc828e3de022d",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/11.578275b3.jpg"
+  },
+  {
+    "revision": "0f113991f7a427fb0bebe95c17851751",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/12.0f113991.jpg"
+  },
+  {
+    "revision": "45c31ae32cc0758b49d12593584c7010",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/13.45c31ae3.jpg"
+  },
+  {
+    "revision": "e4ed42fb17b87acd1cbc3032d34acca3",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/14.e4ed42fb.jpg"
+  },
+  {
+    "revision": "f9b062dd2b80135b8c5d53d017f66136",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/15.f9b062dd.jpg"
+  },
+  {
+    "revision": "9c547c9fe303752dfc3f0b440454fb02",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/16.9c547c9f.jpg"
+  },
+  {
+    "revision": "fb0c6809063c32b9c2f303f8fc3d5f73",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/17.fb0c6809.jpg"
+  },
+  {
+    "revision": "14ae8b2d16c4be90f2448d11b050c0a4",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/18.14ae8b2d.jpg"
+  },
+  {
+    "revision": "a5e26cdcfa4910fdea1d5c2d3678b149",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/19.a5e26cdc.jpg"
+  },
+  {
+    "revision": "fccd5572f401dd9373d353aca4c8da80",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/2.fccd5572.jpg"
+  },
+  {
+    "revision": "6ede44cc7fce01265da8702ac0383e36",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/20.6ede44cc.jpg"
+  },
+  {
+    "revision": "012d1701995c3cd3b7a3b75789a64f97",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/21.012d1701.jpg"
+  },
+  {
+    "revision": "54ef669e3e4efbd7d881ab457b693eeb",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/22.54ef669e.jpg"
+  },
+  {
+    "revision": "d05b6b47c3efad0072d1854a5db435a0",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/3.d05b6b47.jpg"
+  },
+  {
+    "revision": "6fdf0963a41677126af9a88baa17026d",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/4.6fdf0963.jpg"
+  },
+  {
+    "revision": "3c17059b03f3ef09c38622460b35ee7f",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/5.3c17059b.jpg"
+  },
+  {
+    "revision": "f293718290455820baaf3409ae3964fe",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/6.f2937182.jpg"
+  },
+  {
+    "revision": "2949f49b43de3a65b4e63d64cdfa88b5",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/7.2949f49b.jpg"
+  },
+  {
+    "revision": "b37e1a454feffd39c2adec0299c7b202",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/8.b37e1a45.jpg"
+  },
+  {
+    "revision": "99ca7ac17557d8b98cc8291bf50f818e",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/9.99ca7ac1.jpg"
+  },
+  {
+    "revision": "f528a9d355a2408f67f084f30a071ad5",
+    "url": "/Arshiii1425/Responsive-Image-Gallery-Using-React/static/media/Thumbs.f528a9d3.db"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Arshiii1425/Responsive-Image-Gallery-Using-React/precache-manifest.a49c55fde287288a2a6f41a99c9085b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Arshiii1425/Responsive-Image-Gallery-Using-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
