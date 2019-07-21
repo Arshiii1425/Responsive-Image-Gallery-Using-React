94c82e1eadd2eb84c75fc08daed98cdb51a78958
Arshiii <arshiii4425@gmail.com>
2019-07-21 21:12:07 +0500

    Create a responsive image gallery using react js only without any 3rd party library

--- src/App/App.js
+++ src/App/App.js
@@ -1,117 +1,155 @@
-import React ,{Component} from 'react'
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import galleryData from '../media.json';
 import GalleryItem from '../GalleryItem/GalleryItem';
 
-// const targetClassName = "container";
-// const defaultDuration = "1s";
-class App extends Component{
+
+let minHeightWindow = Number.MAX_SAFE_INTEGER;
+const minHeightWindowRatio = 0.25;
+let minHeightRow = Number.MAX_SAFE_INTEGER;
+const targetClassName = "flex-wrap-anim";
+const defaultDuration = "0.5s";
+
+class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {products: galleryData, dummyList : []};
+    this.state = {
+      products: galleryData,
+      dummyList: []
+    };
+
     this.productList = this.productList.bind(this);
-    // this.addDummyList = this.addDummyList.bind(this);  
+    this.addToDummyList = this.addToDummyList.bind(this);
+    this.init = this.init.bind(this);
+    this.init();
+
   }
 
-//   addDummyList(item, duration) {
-//     let top = item.offsetTop;
-//     let left = item.offsetLeft;
-//     let width = item.offsetWidth;
-//     let height = item.offsetHeight;
-//     setTimeout(
-//       function() {
-//         item.style.position = 'absolute';
-//         item.style.top = top + 'px';
-//         item.style.left = left + 'px';
-//         item.style.width = width + 'px';
-//         item.style.height = height + 'px';
-//         let rect = item.getBoundingClientRect();
-//         let dummyDiv = document.createElement('div');
-//         dummyDiv.classList.add(targetClassName + '-dummy');
-//         dummyDiv.style.width = rect.width + 'px';
-//         dummyDiv.style.height = rect.height + 'px';
-//         dummyDiv.style.visibility = 'hidden';
-//         dummyDiv['__' + targetClassName + '_pair'] = item;
-//         dummyDiv['__' + targetClassName + '_duration'] = duration;
-//         item.parentNode.appendChild(dummyDiv);
-//         this.state.dummyList.push(dummyDiv);
-//       }
-//       .bind(this),
-//       0
-//   );
-    
-//   }
+  // This function pre-calculating the minimum height of each row...
 
-//   fetchingData(){
-//   let conts = document.getElementsByClassName(targetClassName);
-//   for (let i = 0, max = conts.length; i < max; i++) {
-//     let cont = conts[i];
-//     cont.style.positoin = 'relative';
-//     let duration = cont.getAttribute('data-duration')
-//       || defaultDuration;
-//     let items = cont.getElementsByClassName('container-item');
-//     for (let i = 0, max = items.length; i < max; i++) {
-//       this.addDummyList(items[i], duration);
-//     }
-//   }
+  init() {
+    minHeightWindow = window.innerHeight;
+    minHeightRow = minHeightWindow * minHeightWindowRatio;
+  }
 
-// }
+  // This function is doing multitask... 
+  //  1. Make the original div to be positioned forcibly to a specific position 
+  //  (so, it is not get re-positioned by the Flex re-positoin mechanism).
+  //  2. Making a dummy div (whose visibility is hidden) per each original div with class 'flex-wrap-anim'
+  //  3. Make the dummy div's size equal to the original div
+  //  4. Add the dummy div to the Flex container div (parent div of the original div)
+  //  5. Add the dummy div to the list
 
-// animateContainer() {
-//   // document.getElementsByClassName('container-item').classList +='container-item2';
-//   this.state.dummyList.forEach(function(dummyDiv) {
-//     let item = dummyDiv['__' + targetClassName + '_pair'];
-//     // let contImages = item.getElementsByClassName('container-img')[0];
-//     // let image = contImages.getElementsByClassName('img')[0];
-//     // console.log(item);
-//     let duration = dummyDiv['__' + targetClassName + '_duration'];
-//     if (item.offsetTop !== dummyDiv.offsetTop) {
-//       console.log('yes');
-//       item.style.transition = 'all ' + duration;
-//       item.style.top = dummyDiv.offsetTop + 'px';
-//       item.style.left = dummyDiv.offsetLeft + 'px';
-//     } else {
-//       item.style.transition = '';
-//       item.style.left = dummyDiv.offsetLeft + 'px';
-//     }
-//   });
-// }
+  addToDummyList(item, duration) {
+    var top = item.offsetTop;
+    var left = item.offsetLeft;
+    setTimeout(function () {
+        item.style.position = "absolute";
+        item.style.left = left + "px";
+        item.style.top = top + "px";
+        var dummyDiv = document.createElement('div');
+        dummyDiv.classList.add(targetClassName + '-dummy');
+        var rect = item.getBoundingClientRect();
+        dummyDiv.style.width = rect.width + 'px';
+        dummyDiv.style.height = rect.height + 'px';
+        dummyDiv.style.visibility = 'hidden';
+        dummyDiv.style.margin = '0.4vmin';
+        dummyDiv.style.flexGrow = 1;
+        dummyDiv['__' + targetClassName + '_pair'] = item;
+        dummyDiv['__' + targetClassName + '_duration'] = duration;
+        item.parentNode.appendChild(dummyDiv);
+        this.state.dummyList.push(dummyDiv);
+      }
+      .bind(this),
+      0
+    );
+  }
 
+  // This is main controller, it controll the image width means pre-define it and 
+  // also it add all the dive to the dummyList function
 
+  mainController() {
+    let conts = document.getElementsByClassName(targetClassName);
+    for (let i = 0, max = conts.length; i < max; i++) {
+      let cont = conts[i];
+      cont.style.position = 'relative';
+      let items = cont.getElementsByTagName('div');
+      for (let i = 0, max = items.length; i < max; i++) {
+        let item = items[i];
+        let img = item.getElementsByTagName('img')[0];
+        let imageWidth = minHeightRow * img.naturalWidth / img.naturalHeight;
+        imageWidth = imageWidth.toFixed(3)
+        item.style.width = imageWidth + "px";
+        // item.style.flexGrow = (img.naturalWidth / img.naturalHeight).toFixed(2);
+        this.addToDummyList(item, defaultDuration);
+      }
+    }
+  }
 
+  // This function traverse the dummylist and check each element either it change it position or not
+  // When window resize occurs, dummy div gets re-positioned without animation,
+  // and forcibly change the original div's position to the dummy div's position
+  //  with transition animation.
+  
 
-// componentDidMount() {
-// 
-//  window.addEventListener("load",this.fetchingData.bind(this));
-//  window.addEventListener("resize", this.animateContainer.bind(this));
-// }
+  reshapeContainer() {
+    this.state.dummyList.forEach(function (dummyDiv) {
+      let item = dummyDiv['__' + targetClassName + '_pair'];
+      let duration = dummyDiv['__' + targetClassName + '_duration'];
+      item.style.transition = 'all ' + duration;
+      item.style.top = dummyDiv.offsetTop + 'px';
+      item.style.left = dummyDiv.offsetLeft + 'px';
+      let rect = dummyDiv.getBoundingClientRect();
+      item.style.width = rect.width * 1.01 + 'px';
+      item.style.height = rect.height + 'px';
+    });
+  }
 
-// componentWillUnmount() {
-//  window.removeEventListener("load",this.fetchingData.bind(this));
-//  window.removeEventListener("resize", this.animateContainer.bind(this));
+  componentDidMount() {
+    window.addEventListener("load", this.mainController.bind(this));
+    window.addEventListener("resize", this.reshapeContainer.bind(this));
+    setInterval(this.reshapeContainer.bind(this), 0);
+  }
 
-// }
+  componentWillUnmount() {
+    window.removeEventListener("load", this.mainController.bind(this));
+    window.removeEventListener("resize", this.reshapeContainer.bind(this));
+  }
 
-productList = () => {
+  // This function is creating a div container for each product i.e for each image and adding it to 
+  // productList
+
+  productList = () => {
     const list = this.state.products.map((product) =>
-          <GalleryItem key ={product.id} imageUrl =  {require('../media/' + product.image )}
-          thumbnail = {require('../media/' + product.thumbnail )}
-          desc = {product.description}
-          />
+      <
+      GalleryItem key = {
+        product.id
+      }
+      imageUrl = {
+        require('../media/' + product.image)
+      }
+      thumbnail = {
+        require('../media/' + product.thumbnail)
+      }
+      desc = {
+        product.description
+      }
+      />
     );
     return (list);
   }
 
   render(){
-    
-    return(
-      <div className="container">
+     return(
+      <div className="container flex-wrap-anim" id="container-all">
         {this.productList()};
         </div>
     );
   }
 
 }
- 
+
 export default App;
